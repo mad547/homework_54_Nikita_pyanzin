dd1aee24a7114be431f6c35a6c5b4eab88d232de
mad547 <97340914+mad547@users.noreply.github.com>
2026-05-31 03:42:50 +0600

    добавил модели Category и Product

--- source/market_app/admin.py
+++ source/market_app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from market_app.models import Category, Product
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description']
+    search_fields = ['title']
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'category', 'price', 'created_at']
+    list_filter = ['category']
+    search_fields = ['title']
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
--- source/market_app/models.py
+++ source/market_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=200, null=False, blank=False, unique=True, verbose_name='Наименование')
+    description = models.TextField(null=True, blank=True, verbose_name='Описание')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'category'
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=200, null=False, blank=False, verbose_name='Наименование')
+    description = models.TextField(null=True, blank=True, verbose_name='Описание')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=False, verbose_name='Категория')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата добавления')
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=False, verbose_name='Стоимость')
+    image = models.URLField(null=False, blank=False, verbose_name='Изображение')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'product'
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
