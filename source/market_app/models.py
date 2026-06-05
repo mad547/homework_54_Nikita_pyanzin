a3c2be82f4cd6c1a8b7720955244f8b847aef8fb
mad547 <97340914+mad547@users.noreply.github.com>
2026-06-06 00:59:41 +0600

    добавил поле stock в модель Product

--- source/market_app/models.py
+++ source/market_app/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
@@ -21,6 +22,11 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата добавления')
     price = models.DecimalField(max_digits=10, decimal_places=2, null=False, verbose_name='Стоимость')
     image = models.URLField(null=False, blank=False, verbose_name='Изображение')
+    stock = models.IntegerField(
+        default=0,
+        validators=[MinValueValidator(0)],
+        verbose_name='Остаток'
+    )
 
     def __str__(self):
         return self.title
