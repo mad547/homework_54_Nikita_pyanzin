72c0cd4266bfabc3e86f933ed84ed61870041be9
mad547 <97340914+mad547@users.noreply.github.com>
2026-06-06 01:54:40 +0600

    обновил views and urls добавил редактирование и удаление

--- source/market_app/urls.py
+++ source/market_app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from market_app.views import products_view, product_view, product_add_view, category_add_view, product_delete_view, product_edit_view
+from market_app.views import (
+    products_view, product_view, product_add_view,
+    product_edit_view, product_delete_view, category_add_view
+)
 
 
 urlpatterns = [
@@ -7,7 +10,7 @@ urlpatterns = [
     path('products/', products_view, name='products'),
     path('products/<int:pk>/', product_view, name='product'),
     path('products/add/', product_add_view, name='product_add'),
-    path('products/<int:pk>/delete/', product_delete_view, name='product_delete'),
     path('products/<int:pk>/edit/', product_edit_view, name='product_edit'),
+    path('products/<int:pk>/delete/', product_delete_view, name='product_delete'),
     path('categories/add/', category_add_view, name='category_add'),
 ]
--- source/market_app/views.py
+++ source/market_app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from market_app.models import Product, Category
+from market_app.forms import ProductForm
 
 
 def products_view(request):
-    products = Product.objects.all()
+    products = Product.objects.filter(stock__gte=1).order_by('category__title', 'title')
     context = {'products': products}
     return render(request, 'market_app/products.html', context)
 
@@ -15,19 +16,37 @@ def product_view(request, pk):
 
 
 def product_add_view(request):
+    form = ProductForm()
     if request.method == 'GET':
-        categories = Category.objects.all()
-        context = {'categories': categories}
-        return render(request, 'market_app/product_add.html', context)
+        return render(request, 'market_app/product_add.html', {'form': form})
     elif request.method == 'POST':
-        product = Product.objects.create(
-            title=request.POST.get('title'),
-            description=request.POST.get('description'),
-            category=Category.objects.get(id=request.POST.get('category')),
-            price=request.POST.get('price'),
-            image=request.POST.get('image'),
-        )
-        return redirect('product', pk=product.pk)
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            product = form.save()
+            return redirect('product', pk=product.pk)
+    return render(request, 'market_app/product_add.html', {'form': form})
+
+
+def product_edit_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    product = ProductForm(instance=product)
+    if request.method == 'GET':
+        return render(request, 'market_app/product_edit.html', {'form': form, 'product': product})
+    elif request.method == 'POST':
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('product', pk=product.pk)
+        return render(request, 'market_app/product_edit.html', {'form': form, 'product': product})
+
+
+def product_delete_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        product.delete()
+        return redirect('products')
+    return render(request, 'market_app/product_confirm_delete.html', {'product': product})
+
 
 def category_add_view(request):
     if request.method == 'GET':
@@ -38,25 +57,3 @@ def category_add_view(request):
             description=request.POST.get('description'),
         )
         return redirect('products')
-
-
-def product_delete_view(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    product.delete()
-    return redirect('products')
-
-
-def product_edit_view(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    if request.method == 'GET':
-        categories = Category.objects.all()
-        context = {'product': product, 'categories': categories}
-        return render(request, 'market_app/product_edit.html', context)
-    elif request.method == 'POST':
-        product.title = request.POST.get('title')
-        product.description = request.POST.get('description')
-        product.category = Category.objects.get(id=request.POST.get('category'))
-        product.price = request.POST.get('price')
-        product.image = request.POST.get('image')
-        product.save()
-        return redirect('product', pk=product.pk)
